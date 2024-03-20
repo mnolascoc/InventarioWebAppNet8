a791cdb3ed82e92bd9a580084e2b65fbe196d778
Miguel Nolasco <mnolascoc@outlook.com>
2024-03-19 22:40:22 -0500

    Proyecto inicializado

---
+++ InventarioWebappNet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventarioWebappNet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
